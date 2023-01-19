86133eff3438700f5e364d2359eee2d34c59a496
purplekev <101190391+purplekev@users.noreply.github.com>
2023-01-19 17:28:37 +1100

    updating essentials

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const pics = [
     'url("1.jpg")',
-    'url("2.jpg")',
-    'url("3.jpg")',
-    'url("4.jpg")',
-    'url("5.jpg")',
-]
+    // 'url("2.jpg")',
+    // 'url("3.jpg")',
+    // 'url("4.jpg")',
+    // 'url("5.jpg")',
+];
+
+const pic = document.querySelector('section');
+
+function showImage(){
+    var a = Math.floor(Math.random()*pics.length);
+    var img = pics[a];
+    pic.style.backgroundImage = img;
+}
