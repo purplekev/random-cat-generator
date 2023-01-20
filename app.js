e3937fcef577fa959b59a9686cf589094b2ee4d7
purplekev <101190391+purplekev@users.noreply.github.com>
2023-01-20 13:51:13 +1100

    updating files

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const pics = [
-    'url("1.jpg")',
-    // 'url("2.jpg")',
-    // 'url("3.jpg")',
-    // 'url("4.jpg")',
-    // 'url("5.jpg")',
+    'url(1.jpg)',
+    'url(2.jpg)',
+    'url(3.jpg)',
 ];
 
 const pic = document.querySelector('section');
 
 function showImage(){
     var a = Math.floor(Math.random()*pics.length);
+    console.log(a);
     var img = pics[a];
+    console.log(img);
     pic.style.backgroundImage = img;
 }
